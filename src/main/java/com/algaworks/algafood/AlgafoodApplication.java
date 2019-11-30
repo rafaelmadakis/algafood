28e29f30d868dfff33ed66f1bd7c22fbd55bedd7
rafaelmadakis <rafaelmadakis@gmail.com>
2019-11-29 21:53:04 -0300

    Commit Inicial

--- src/main/java/com/algaworks/algafood/AlgafoodApplication.java
+++ src/main/java/com/algaworks/algafood/AlgafoodApplication.java
@@ -8,6 +8,9 @@ public class AlgafoodApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AlgafoodApplication.class, args);
+
+
+        System.out.println("TEste");
     }
 
 }
