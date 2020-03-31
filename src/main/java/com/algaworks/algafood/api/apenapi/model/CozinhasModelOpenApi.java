b22cc76ee75cf3b919781d3c85f1d55512916712
rafaelmadakis <rafaelmadakis@gmail.com>
2020-03-30 22:02:34 -0300

    Correção da documentação de cozinhas, apos Hateoas

--- src/main/java/com/algaworks/algafood/api/apenapi/model/CozinhasModelOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/apenapi/model/CozinhasModelOpenApi.java
@@ -2,8 +2,29 @@ package com.algaworks.algafood.api.apenapi.model;
 
 import com.algaworks.algafood.api.model.CozinhaModel;
 import io.swagger.annotations.ApiModel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.hateoas.Links;
+
+import java.util.List;
 
 @ApiModel("CozinhasModel")
-public class CozinhasModelOpenApi extends PagedModelOpenApi<CozinhaModel> {
+@Setter
+@Getter
+public class CozinhasModelOpenApi  {
+
+    private CozinhasModelOpenApi.CozinhasEmbeddedModelOpenApi _embedded;
+    private Links _links;
+    private PageModelOpenApi page;
+
+    @ApiModel("CidadesEmbeddedModel")
+    @Data
+    public class CozinhasEmbeddedModelOpenApi {
+
+        private List<CozinhaModel> cozinhas;
+
+    }
+
 
 }
