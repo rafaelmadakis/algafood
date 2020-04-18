c3d367e28eb5ccaac659ea4366989c3f711bc43c
rafaelmadakis <rafaelmadakis@gmail.com>
2020-04-17 23:38:29 -0300

    Carregando Granted Authorities dos escopos do OAuth2 no Resource Server

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,14 +1,16 @@
 package com.algaworks.algafood.core.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.stream.Collectors;
 
@@ -41,9 +43,14 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                  authorities = Collections.emptyList();
              }
 
-             return authorities.stream()
+             var scopesAuthritiesConverter = new JwtGrantedAuthoritiesConverter();
+             Collection<GrantedAuthority> grantedAuthorities = scopesAuthritiesConverter.convert(jwt);
+
+             grantedAuthorities.addAll(authorities.stream()
                      .map(SimpleGrantedAuthority::new)
-                     .collect(Collectors.toList());
+                     .collect(Collectors.toList()));
+
+             return grantedAuthorities;
          });
 
          return jwtAuthenticationConverter;
