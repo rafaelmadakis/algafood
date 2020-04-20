11b32dab834339450b1121b6736a823bd60eb6d3
rafaelmadakis <rafaelmadakis@gmail.com>
2020-04-19 22:37:16 -0300

    Restringindo acessos com @PostAuthorize

--- src/main/java/com/algaworks/algafood/core/security/AlgaSecurity.java
+++ src/main/java/com/algaworks/algafood/core/security/AlgaSecurity.java
@@ -26,4 +26,5 @@ public class AlgaSecurity {
     public boolean gerenciaRestaurantes(Long restauranteId) {
         return  restauranteRepository.existsResponsavel(restauranteId, getUsuarioId());
     }
+
 }
--- src/main/java/com/algaworks/algafood/core/security/CheckSecurity.java
+++ src/main/java/com/algaworks/algafood/core/security/CheckSecurity.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.core.security;
 
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.lang.annotation.ElementType;
@@ -40,13 +41,25 @@ public @interface CheckSecurity {
         public @interface PodeGerenciarFuncionamento {
         }
 
+
         @PreAuthorize("hasAuthority('SCOPE_READ') and isAuthenticated()")
         @Retention(RetentionPolicy.RUNTIME)
         @Target(ElementType.METHOD)
-        public @interface  PodeConsultar {
+        public @interface PodeConsultar {
         }
 
+    }
 
+    public @interface Pedidos {
+
+        @PreAuthorize("hasAuthority('SCOPE_READ') and isAuthenticated()")
+        @PostAuthorize("hasAuthority('CONSULTAR_PEDIDOS') or "
+                + "@algaSecurity.getUsuarioId() == returnObject.cliente.id or"
+                + "@algaSecurity.gerenciaRestaurantes(returnObject.restaurante.id)")
+        @Retention(RetentionPolicy.RUNTIME)
+        @Target(ElementType.METHOD)
+        public @interface PodeBuscar {
+        }
 
     }
 
