7e7687e7a9e335ac79c9e49b24b7acc2d7b56fbe
rafaelmadakis <rafaelmadakis@gmail.com>
2020-04-21 00:23:36 -0300

    Restringindo acessos aos endpoints de estatísticas

--- src/main/java/com/algaworks/algafood/api/v1/controller/EstatisticasController.java
+++ src/main/java/com/algaworks/algafood/api/v1/controller/EstatisticasController.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.api.v1.controller;
 
 import com.algaworks.algafood.api.v1.AlgaLinks;
 import com.algaworks.algafood.api.openapi.controller.EstatisticaControllerOpenApi;
+import com.algaworks.algafood.core.security.CheckSecurity;
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 import com.algaworks.algafood.domain.service.VendaQueryService;
@@ -32,6 +33,7 @@ public class EstatisticasController implements EstatisticaControllerOpenApi {
     private AlgaLinks algaLinks;
 
 
+    @CheckSecurity.Estatisticas.PodeConsultar
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public EstatisticasModel estatisticas() {
         var estatisticasModel = new EstatisticasModel();
@@ -41,6 +43,7 @@ public class EstatisticasController implements EstatisticaControllerOpenApi {
         return estatisticasModel;
     }
 
+    @CheckSecurity.Estatisticas.PodeConsultar
     @Override
     @GetMapping(path = "/vendas-diarias", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro,
@@ -48,6 +51,7 @@ public class EstatisticasController implements EstatisticaControllerOpenApi {
         return vendaQueryService.consultaVendasDiarias(filtro,timeOffset);
     }
 
+    @CheckSecurity.Estatisticas.PodeConsultar
     @Override
     @GetMapping(path = "/vendas-diarias", produces = MediaType.APPLICATION_PDF_VALUE)
     public ResponseEntity<byte[]> consultarVendasDiariasPdf(VendaDiariaFilter filtro,
--- src/main/java/com/algaworks/algafood/core/security/CheckSecurity.java
+++ src/main/java/com/algaworks/algafood/core/security/CheckSecurity.java
@@ -167,4 +167,19 @@ public @interface CheckSecurity {
 
 
 
+    public @interface Estatisticas {
+
+        @PreAuthorize("hasAuthority('SCOPE_READ') and "
+                + "hasAuthority('GERAR_RELATORIOS')")
+        @Retention(RetentionPolicy.RUNTIME)
+        @Target(ElementType.METHOD)
+        public @interface PodeConsultar {
+
+        }
+    }
+
+
+
+
+
 }
