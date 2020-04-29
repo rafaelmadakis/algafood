36e63fe267a3c26d3f00002ca5b23fd3ee453563
rafaelmadakis <rafaelmadakis@gmail.com>
2020-04-29 01:29:15 -0300

    correção migração

--- src/main/resources/db/migration/V001__criacao-inicial.sql
+++ src/main/resources/db/migration/V001__criacao-inicial.sql
@@ -1,6 +1,6 @@
 create table cozinha (
-                         id bigint not null auto_increment,
-                         nome varchar(60) not null,
+  id bigint not null auto_increment,
+  nome varchar(60) not null,
 
-                         primary key (id)
+  primary key (id)
 ) engine=InnoDB default charset=utf8;
